92c9c3b832c112ff7dcaa3b68cff0741ed6f59eb
Jonathan Jacka <jonathanjacka@gmail.com>
2021-07-10 13:44:02 -0500

    Updated list display menu items for side drawer

--- src/components/header_footer/SideDrawer.js
+++ src/components/header_footer/SideDrawer.js
@@ -3,7 +3,23 @@ import React from 'react';
 import { Drawer, List, ListItem } from '@material-ui/core';
 
 
-const SideDrawer = ( {open, handleClick } ) => {
+const SideDrawer = ( { open, handleClick } ) => {
+
+    const links = [
+        { where: 'featured', value: 'To top' },
+        { where: 'venueInfo', value: 'Venue Info' },
+        { where: 'highlights', value: 'Highlights' },
+        { where: 'pricing', value: 'Pricing' },
+        { where: 'location', value: 'Location' }
+    ];
+
+    const renderItem = (item, index) => 
+        (
+            <ListItem onClick={() => alert('where: ' + item.where)} key={index} style={{cursor: 'pointer'}}>
+                {item.value}
+            </ListItem>
+        );
+    
      return (
         <Drawer 
             anchor="right"
@@ -11,9 +27,7 @@ const SideDrawer = ( {open, handleClick } ) => {
             onClose={handleClick}
         >
             <List component="nav">
-                <ListItem onClick={() => alert('click')} style={{cursor: 'pointer'}}>
-                    Name of Item 1
-                </ListItem>
+                { links.map((item, index) => renderItem(item, index)) }
             </List>
         </Drawer>
      );
