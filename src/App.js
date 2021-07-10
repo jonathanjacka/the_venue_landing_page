489cdd825b74a7b907a01416682a903fbd85830f
Jonathan Jacka <jonathanjacka@gmail.com>
2021-07-10 14:25:44 -0500

    Updated header.js to change on scroll

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ const App = () => {
   return (
     <div className='App'>
       <Header />
+
+      <div style={ { backgroundColor: 'red', height: '800px' } }></div>
+      <div style={ { backgroundColor: 'blue', height: '800px' } }></div>
+      <div style={ { backgroundColor: 'green', height: '800px' } }></div>
+      <div style={ { backgroundColor: 'yellow', height: '800px' } }></div>
+
     </div>
   );
 };
--- src/components/header_footer/header.js
+++ src/components/header_footer/header.js
@@ -6,24 +6,32 @@ import SideDrawer from './SideDrawer';
 const Header = () => {
 
   const [ drawerOpen, setDrawerOpen ] = useState(false);
+  const [ headerShow, setHeaderShow ] = useState(false);
 
-  //open and close sideDrawer on Menu icon click
   const handleClick = () => {
     drawerOpen ? setDrawerOpen(false) : setDrawerOpen(true);
   }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    window.scrollY > 10 ? setHeaderShow(true) : setHeaderShow(false);
+  };
   
   return (
     <AppBar 
       position="fixed"
       style={{ 
-        backgroundColor: '#2f2f2f', 
+        backgroundColor: headerShow ? '#2f2f2f' : 'transparent',
         boxShadow: 'none',
         padding: '10px 0'
       }}
     >
       <Toolbar>
         <div className="header_logo">
-          <div className="font_righteous header_logo_venue">The Venue</div>
+          <div className="font_righteous header_logo_venue">The Rock Show</div>
           <div className="header_logo_title">Musical Events</div>
         </div>
 
