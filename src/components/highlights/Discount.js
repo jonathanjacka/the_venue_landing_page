e3da240b0ebee976cfcdc5148f3d2764c694ecc7
Jonathan Jacka <jonathanjacka@gmail.com>
2021-07-12 14:32:32 -0500

    Updated and completed Discount component with percentage ticker

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Fade, Slide } from 'react-awesome-reveal';
 
 const Discount = () => {
+
+    const endPercentage = 30;
+
+    const [ percentage , setPercentage ] = useState(0);
+
+    const updatePercent = () => {
+        if (percentage < endPercentage){
+            setPercentage(prev => prev + 1);
+        }
+    }
+
+    useEffect( () => {
+        if(percentage > 0 && percentage < 30){
+            setTimeout(() => {
+                setPercentage(prev => prev + 1);
+            }, 30);
+        }
+    }, [percentage]);
+
     return(
-        <div>Discount</div>
+        <div className="center_wrapper">
+            <div className="discount_wrapper">
+
+            <Fade
+                onVisibilityChange={ (inView) => {
+                    if(inView) {
+                        updatePercent();
+                    }
+                }}
+            >
+                <div className="discount_percentage">
+                    <span>{`${percentage}%`}</span>
+                    <span>Off</span>
+                </div>
+            </Fade>
+
+            <Slide right={true}>
+                <div className="discount_description">
+                    <h3>Early Bird Ticket Price </h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </Slide>
+
+            </div>
+        </div>
     );
 }
 
